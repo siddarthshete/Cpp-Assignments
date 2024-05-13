4a613f517c7aff62d5887bc050b0506a0b12860b
siddarthshete <148082976+siddarthshete@users.noreply.github.com>
2024-05-13 13:39:01 +0530

    Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,35 +1,97 @@
-#include<iostream>
+/*
+Assignment No: 02
+Problem Statement :
+ Write a cpp program to create class DM which stores the value 
+of distance in meter and centimeters. Read the values for a class object and find 
+greatest
+ distance from two objects.Use friend function to carry the addition
+operation. The display should be in format of meters and centimeter.
+Name:Shete Siddarth Sudhir
+Class: FYC Roll no: 61
+*/
+#include<iostream> // Header files
 using namespace std;
-class DM{
-    private: 
-    long int m;
-    long int cm;
-    public:
-    void take(){
-        cout<<"Enter value in meter:";
-        cin>>m;
-
-    }
-    friend DM add(DM m,DM cm);
-};
-DM add (DM S1,DM S2)
+class DM //class declaration
 {
-    int tm,tcm;
-    tcm=(S1.m*100+S2.m*100);
-    tm=tcm/100;
-    cout<<tm<< " meter"<<endl;
-    cout<<tcm<<" centimeter"<<endl;
-
+public: //access specifier
+int meter;
+int centimeter;
+public:
+DM()
+{
+meter=0;
+centimeter=0;
+}
+DM(int m,int cm)
+{
+meter=m;
+centimeter=cm;
+}
+friend DM add(DM d1,DM d2) //friend function
+{
+int cm=d1.centimeter+d2.centimeter;
+int m=d1.meter+d2.meter;
+cm=cm%100;
+return DM(m,cm);
+}
+void putdata() //member function
+{
+cout<<"Meter:"<<meter<<endl;
+cout<<"Centimeter:"<<centimeter<<endl;
+}
+friend DM great(DM d1,DM d2) //friend function
+{
+float dist1=d1.meter*100+d1.centimeter;
+float dist2=d2.meter*100+d2.centimeter;
+cout<<"greater object:\n"; //condition forchecking greater distance
+if(dist1>dist2)
+{
+cout<<"first object is greater\n";
+}
+else
+{
+cout<<"second object is greater\n";
+}
+return DM(dist1,dist2);
+}
 };
 int main()
 {
-    DM S1, S2;
-    cout<<"Enter distance 1:"<<endl;
-    S1.take();
-    cout<<"Enter distance 2:"<<endl;
-    S2.take();
-    cout<<"The Total distance is :"<<endl;
-    add(S1,S2);
-    return 0;
+int m1,m2,cm1,cm2;
+cout<<"Enter the Distance in Meter for First object:";
+cin>>m1;
+cout<<"Enter the Distance in Centimeter for First object:";
+cin>>cm1;
+cout<<"Enter the Distance in Meter for second object:";
+cin>>m2;
+cout<<"Enter the Distance in Centimeter for second object:";
+cin>>cm2;
+DM d1(m1,cm1);
+DM d2(m2,cm2);
+DM d3=add(d1,d2);
+cout<<"Distance of first object\n";
+d1.putdata();
+cout<<"Distance of second object\n";
+d2.putdata();
+cout<<"Total distance:\n";
+d3.putdata();
+great(d1,d2);
+return 0;
 }
-
+/*Output:
+Enter the Distance in Meter for First object:12
+Enter the Distance in Centimeter for First object:20
+Enter the Distance in Meter for second object:8
+Enter the Distance in Centimeter for second object:40
+Distance of first object
+Meter:12
+Centimeter:20
+Distance of second object
+Meter:8
+Centimeter:40
+Total distance:
+Meter:20
+Centimeter:60
+greater object:
+first object is greater
+*/
